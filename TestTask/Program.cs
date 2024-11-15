cb1cf7831e7972223b1f94fb1b16ece14e6a446c
AshRaven521 <ivan.kondrashov.92@mail.ru>
2024-11-15 17:16:52 +0300

    update: fix 500 error because of json parsing

--- TestTask/Program.cs
+++ TestTask/Program.cs
@@ -1,11 +1,25 @@
 using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
 using TestTask.Data;
+using TestTask.Services.Implementations;
+using TestTask.Services.Implementations.DataAccessLayer;
+using TestTask.Services.Interfaces;
+using TestTask.Services.Interfaces.DataAccessLayer;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<IOrderRepository, OrderRepository>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IOrderService, OrderService>();
+builder.Services.AddScoped<IUserService, UserService>();
+
 
 builder.Services.AddControllers();
+// Fix 500 error with json parsing
+builder.Services.AddControllers().AddJsonOptions(x =>
+                x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
